84e25de3533ee35fc4c2650737af6326ee38836c
matt <opey@gmx.de>
2016-09-14 01:46:32 +0200

    - add first draft of video implementation - extend config

--- src/slidePlayer-config.js
+++ src/slidePlayer-config.js
@@ -1,1 +1,22 @@
-var spConfig = {};
+window.spConfig = {
+    debug: false,
+    useAnimation: true,
+    aspectRatio: '16:9',
+    videoPaths: {
+        small: {
+            mp4: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+            ogg: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
+            webm: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm'
+        },
+        medium: {
+            mp4: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+            ogg: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
+            webm: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm'
+        },
+        large: {
+            mp4: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
+            ogg: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.ogv',
+            webm: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm'
+        }
+    }
+};
--- src/slidePlayer.js
+++ src/slidePlayer.js
@@ -1,18 +1,61 @@
-var slidePlayer = (function (window, document) {
-    console.log(document.querySelector('#slidePlayer'));
+window.slidePlayer = (function (window, document) {
+
+    var config = {};
+    var container = null;
+    var video = null;
 
 
     function init() {
-        console.log('init');
+        config = window.spConfig;
+        container = document.querySelector('#slidePlayer');
+
+        setupPlayerDomStructure();
+    }
+
+    function setupPlayerDomStructure() {
+        var innerContainer = document.createElement('div');
+        innerContainer.style.height = '0px';
+        innerContainer.style.position = 'relative';
+        innerContainer.style.paddingTop = getAspectRatioAsPercent(config.aspectRatio);
+        container.appendChild(innerContainer);
+
+        var video = document.createElement('video');
+        video.setAttribute('src', getVideoSource());
+        video.style.position = 'absolute';
+        video.style.height = video.style.width = '100%';
+        video.style.top = video.style.left = '0px';
+        innerContainer.appendChild(video);
+    }
+
+    function getVideoSource() {
+        return config.videoPaths[getCurrentMediaState()][getSupportedMediaType()];
+    }
+
+    function getCurrentMediaState() {
+        // TODO return correct MediaState ('small'|'medium'|'large') according to window width
+        return 'medium';
+    }
+
+    function getSupportedMediaType() {
+        // TODO return correct MediaType ('mp4'|'ogg'|'webm') according to browser support
+        return 'webm';
+    }
+
+    function getAspectRatioAsPercent(ratio) {
+        if (ratio && !!~(ratio.indexOf(':'))) {
+            return (ratio.split(':')[1] / ratio.split(':')[0]) * 100 + '%';
+        }
+        console.warn('No correct aspectRatio is provided. Will use \'16:9\' instead.');
+        return getAspectRatioAsPercent('16:9');
     }
 
     function domReady(fn) {
-        if (/in/.test(document.readyState)) {
+        if ( document.readyState === 'complete' && window.spConfig) {
+            fn();
+        } else {
             setTimeout(function () {
                 domReady(fn);
             }, 9)
-        } else {
-            fn();
         }
     }
 
