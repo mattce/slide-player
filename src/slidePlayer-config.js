94e7a92cf923c0c28ed451a8441deece0e6cdc32
matt <opey@gmx.de>
2016-09-17 14:27:09 +0200

    - structure application into logical parts. use prototypes instead of plain functions - add breakpoint data

--- src/slidePlayer-config.js
+++ src/slidePlayer-config.js
@@ -2,6 +2,10 @@ window.spConfig = {
     debug: false,
     useAnimation: true,
     aspectRatio: '16:9',
+    breakPoints: {
+        medium: 728,
+        large: 1024
+    },
     videoPaths: {
         small: {
             mp4: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',
--- src/slidePlayer.js
+++ src/slidePlayer.js
@@ -1,65 +1,138 @@
-window.slidePlayer = (function (window, document) {
-
-    var config = {};
-    var container = null;
-    var video = null;
+;(function (window, document) {
 
 
-    function init() {
-        config = window.spConfig;
-        container = document.querySelector('#slidePlayer');
+    // BROWSER UTILS
 
-        setupPlayerDomStructure();
-    }
+    var BrowserUtilsInstance = (function () {
+        var instance = null;
 
-    function setupPlayerDomStructure() {
-        var innerContainer = document.createElement('div');
-        innerContainer.style.height = '0px';
-        innerContainer.style.position = 'relative';
-        innerContainer.style.paddingTop = getAspectRatioAsPercent(config.aspectRatio);
-        container.appendChild(innerContainer);
+        var BrowserUtils = function () {
+            console.log('Browser here');
+            this.config = window.spConfig;
+        };
 
-        var video = document.createElement('video');
-        video.setAttribute('src', getVideoSource());
-        video.style.position = 'absolute';
-        video.style.height = video.style.width = '100%';
-        video.style.top = video.style.left = '0px';
-        innerContainer.appendChild(video);
-    }
+        BrowserUtils.prototype.constructor = BrowserUtils;
 
-    function getVideoSource() {
-        return config.videoPaths[getCurrentMediaState()][getSupportedMediaType()];
-    }
+        BrowserUtils.prototype.watchWindowSize = function () {
+            window.addEventListener('resize', Helpers.debounce(function () {
+                console.log(window.innerWidth);
+            }, 300), false);
+        };
 
-    function getCurrentMediaState() {
-        // TODO return correct MediaState ('small'|'medium'|'large') according to window width
-        return 'medium';
-    }
+        BrowserUtils.prototype.getCurrentMediaState = function () {
+            // TODO return correct MediaState ('small'|'medium'|'large') according to window width
+            return 'medium';
+        };
 
-    function getSupportedMediaType() {
-        // TODO return correct MediaType ('mp4'|'ogg'|'webm') according to browser support
-        return 'webm';
-    }
+        BrowserUtils.prototype.getSupportedMediaType = function () {
+            // TODO return correct MediaType ('mp4'|'ogg'|'webm') according to browser support
+            return 'webm';
+        };
 
-    function getAspectRatioAsPercent(ratio) {
-        if (ratio && !!~(ratio.indexOf(':'))) {
-            return (ratio.split(':')[1] / ratio.split(':')[0]) * 100 + '%';
+        return {
+            getInstance: function () {
+                if (!instance) {
+                    instance = new BrowserUtils();
+                }
+                return instance;
+            }
         }
-        console.warn('No correct aspectRatio is provided. Will use \'16:9\' instead.');
-        return getAspectRatioAsPercent('16:9');
-    }
 
-    function domReady(fn) {
-        if ( document.readyState === 'complete' && window.spConfig) {
-            fn();
-        } else {
-            setTimeout(function () {
-                domReady(fn);
-            }, 9)
+    })();
+
+
+    // SLIDE PLAYER
+
+    var SlidePlayerInstance = (function () {
+        var instance = null;
+
+        function SlidePlayer() {
+            console.log('SlidePlayer here');
+            this.config = window.spConfig;
+            this.video = null;
+            this.container = document.querySelector('#slidePlayer');
+            this.BrowserUtils = BrowserUtilsInstance.getInstance();
+
+            this.setupPlayerDomStructure();
         }
-    }
 
-    domReady(init);
+        SlidePlayer.prototype.constructor = SlidePlayer;
 
-})
-(window, document);
+        SlidePlayer.prototype.setupPlayerDomStructure = function () {
+            var innerContainer = document.createElement('div');
+            innerContainer.style.height = '0px';
+            innerContainer.style.position = 'relative';
+            innerContainer.style.paddingTop = Helpers.getAspectRatioAsPercent(this.config.aspectRatio);
+            this.container.appendChild(innerContainer);
+
+            this.video = document.createElement('video');
+            this.video.setAttribute('src', this.getVideoSource());
+            this.video.style.position = 'absolute';
+            this.video.style.height = this.video.style.width = '100%';
+            this.video.style.top = this.video.style.left = '0px';
+            innerContainer.appendChild(video);
+        };
+
+        SlidePlayer.prototype.getVideoSource = function () {
+            var currentMediaState = this.BrowserUtils.getCurrentMediaState();
+            var supportedMediaType = this.BrowserUtils.getSupportedMediaType();
+            return this.config.videoPaths[currentMediaState][supportedMediaType];
+        };
+
+        return {
+            getInstance: function () {
+                if (!instance) {
+                    instance = new SlidePlayer();
+                }
+                return instance;
+            }
+        }
+
+    })();
+
+    var Helpers = (function () {
+
+        function domReady(fn) {
+            if (document.readyState === 'complete' && window.spConfig) {
+                fn();
+            } else {
+                setTimeout(function () {
+                    domReady(fn);
+                }, 9)
+            }
+        }
+
+        function debounce(func, wait, immediate) {
+            var timeout;
+            return function () {
+                var context = this, args = arguments;
+                var later = function () {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+            };
+        }
+
+        function getAspectRatioAsPercent(ratio) {
+            if (ratio && !!~(ratio.indexOf(':'))) {
+                return (ratio.split(':')[1] / ratio.split(':')[0]) * 100 + '%';
+            }
+            console.warn('No correct aspectRatio is provided. Will use \'16:9\' instead.');
+            return getAspectRatioAsPercent('16:9');
+        }
+
+        return {
+            domReady: domReady,
+            debounce: debounce,
+            getAspectRatioAsPercent: getAspectRatioAsPercent
+        }
+
+    })();
+
+    Helpers.domReady(function () {
+        SlidePlayerInstance.getInstance();
+    });
+
+})(window, document);
