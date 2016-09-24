b8a8acd14ddf9e50cb698290558a05f032a66d00
matt <opey@gmx.de>
2016-09-24 16:19:17 +0200

    - add first draft of VideoInstance - add first draft of PosterInstance - add Ticker - extend Helpers with: - - getUniqueId() - - getObjectSize() - implement pre-loader mechanics for domReady and posterReady - add first draw of scenes concept

--- src/slidePlayer-config.js
+++ src/slidePlayer-config.js
@@ -1,5 +1,5 @@
 window.spConfig = {
-    debug: false,
+    debug: true,
     useAnimation: true,
     aspectRatio: '16:9',
     breakPoints: {
@@ -29,5 +29,59 @@ window.spConfig = {
             vp9: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm',
             hls: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.webm'
         }
-    }
+    },
+    scenes: [
+        {
+            id: 'intro',
+            // poster: 'http://www.androidcentral.com/sites/androidcentral.com/files/styles/w550h500/public/wallpapers/blurry-lights-2to.jpg',
+            poster: 'https://a2ua.com/blurry/blurry-005.jpg',
+            enter: {
+                start: '00:00',
+                end: '00:02'
+            },
+            exit: {
+                start: '00:10',
+                end: '00:12'
+            },
+            hotSpots: [
+                {
+                    id: 'test-1',
+                    action: '#test',
+                    width: '10%',
+                    height: '10%',
+                    positionX: '5%',
+                    positionY: '5%',
+                    customClass: 'custom-class'
+                },
+                {
+                    id: 'test-2',
+                    action: '.test',
+                    width: '10%',
+                    height: '10%',
+                    positionX: '80%',
+                    positionY: '20%'
+                },
+                {
+                    id: 'test-3',
+                    action: 'zoom',
+                    width: '10%',
+                    height: '10%',
+                    positionX: '50%',
+                    positionY: '50%'
+                }
+            ]
+        },
+        {
+            poster: 'https://a2ua.com/blurry/blurry-004.jpg'
+        },
+        {
+            poster: 'https://a2ua.com/blurry/blurry-002.jpg'
+        },
+        {
+            poster: 'https://a2ua.com/blurry/blurry-003.jpg'
+        },
+        {
+            poster: 'https://a2ua.com/blurry/blurry-012.jpg'
+        }
+    ]
 };
--- src/slidePlayer.js
+++ src/slidePlayer.js
@@ -11,7 +11,7 @@
         var instance = null;
 
         function SlidePlayer() {
-            console.log('SlidePlayer here');
+            console.log('SlidePlayer');
             var _this = this;
 
             this.config = window[configSlug];
@@ -24,12 +24,14 @@
             }, false);
 
             this.setupPlayerDomStructure();
+
+            return this;
         }
 
         SlidePlayer.prototype.constructor = SlidePlayer;
 
         SlidePlayer.prototype.onResize = function (data) {
-            console.log(data);
+            // console.log(data);
         };
 
         SlidePlayer.prototype.setupPlayerDomStructure = function () {
@@ -39,19 +41,19 @@
             innerContainer.style.paddingTop = Helpers.getAspectRatioAsPercent(this.config.aspectRatio);
             this.container.appendChild(innerContainer);
 
-            this.video = document.createElement('video');
-            this.video.style.position = 'absolute';
-            this.video.style.height = this.video.style.width = '100%';
-            this.video.style.top = this.video.style.left = '0px';
-            this.video.setAttribute('src', this.getVideoSource());
+            var video = document.createElement('video');
+            video.style.position = 'absolute';
+            video.style.height = video.style.width = '100%';
+            video.style.top = video.style.left = '0px';
+            video.setAttribute('src', this.getVideoSource());
             if (this.config.debug) {
-                this.video.setAttribute('controls', true);
+                video.setAttribute('controls', 'true');
             }
-            innerContainer.appendChild(this.video);
+            this.video = VideoInstance.getInstance(innerContainer.appendChild(video));
         };
 
         SlidePlayer.prototype.getVideoSource = function () {
-            if (!Helpers.doesObjectExist(this.config.videoPaths)) {
+            if (!~(Helpers.doesObjectExist(this.config.videoPaths))) {
                 throw new Error('No video paths defined in config!');
             }
             var breakPointState = this.BrowserUtils.getBreakPointState();
@@ -71,22 +73,99 @@
     })();
 
 
+    // VIDEO
+
+    var VideoInstance = (function () {
+        var instance = null;
+
+        function Video(videoElement) {
+            console.log('Video');
+            this.elem = videoElement;
+            return this;
+        }
+
+        Video.prototype.gotoAndPlay = function (start, end) {
+            this.elem.play();
+        };
+
+        return {
+            getInstance: function (param) {
+                if (!instance) {
+                    instance = new Video(param);
+                }
+                return instance;
+            }
+        }
+
+    })();
+
+
+    // POSTER
+
+    var PostersInstance = (function () {
+        var instance = null;
+
+        function Poster() {
+            console.log('Poster');
+            return this;
+        }
+
+
+        Poster.prototype.posterReady = function (fn) {
+            var scenes = window.spConfig.scenes,
+                index = 0,
+                length = scenes.length;
+            var id = Helpers.getUniqueId();
+            for (; index < length; index++) {
+                var img = new Image();
+                img.src = scenes[index].poster + '?' + ns;
+                img.onload = function () {
+                    index--;
+                    console.log(this);
+                };
+            }
+            Ticker.registerTask(id, function () {
+                if (index === 0) {
+                    fn();
+                    Ticker.removeTask(id);
+                }
+            });
+
+        };
+
+        return {
+            getInstance: function (param) {
+                if (!instance) {
+                    instance = new Poster(param);
+                }
+                return instance;
+            }
+        }
+
+    })();
+
+
     // BROWSER UTILS
 
     var BrowserUtilsInstance = (function () {
         var instance = null;
 
-        var BrowserUtils = function () {
-            console.log('Browser here');
+        function BrowserUtils() {
+            console.log('Browser');
             this.config = window[configSlug];
             this.lastBreakPoint = null;
             this.currentBreakPoint = 'N/A';
-
+            var _this = this;
             this.watchWindowSize();
             this.setBreakPointState();
-            // TODO : investigate why this is not thrown on init
-            Helpers.dispatchCustomEvent('utils/resize', {breakPointData: this.getBreakPointState()});
-        };
+
+            // dispatching the event without a delay isn't captured
+            setTimeout(function () {
+                Helpers.dispatchCustomEvent('utils/resize', {breakPointData: _this.getBreakPointState()});
+            }, 1);
+
+            return this;
+        }
 
         BrowserUtils.prototype.constructor = BrowserUtils;
 
@@ -101,15 +180,17 @@
         };
 
         BrowserUtils.prototype.setBreakPointState = function () {
-            if (!Helpers.doesObjectExist(this.config.breakPoints)) {
+            if (!~(Helpers.doesObjectExist(this.config.breakPoints))) {
                 console.warn('No breakPoints defined. Will use \'{small: 0}\' instead.');
                 this.config.breakPoints = {small: 0};
             }
             this.lastBreakPoint = this.currentBreakPoint;
             var browserWidth = this.getBrowserWidth();
             for (var breakpoint in this.config.breakPoints) {
-                if (this.config.breakPoints[breakpoint] < browserWidth) {
-                    this.currentBreakPoint = breakpoint;
+                if (this.config.breakPoints.hasOwnProperty(breakpoint)) {
+                    if (this.config.breakPoints[breakpoint] < browserWidth) {
+                        this.currentBreakPoint = breakpoint;
+                    }
                 }
             }
         };
@@ -126,6 +207,8 @@
         };
 
         BrowserUtils.prototype.getSupportedMediaType = function () {
+            // alo see: http://stackoverflow.com/questions/7451635/how-to-detect-supported-video-formats-for-the-html5-video-tag
+            // and: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/video.js#L38
             var elem = document.createElement('video'),
                 types = {
                     ogg: 'video/ogg; codecs="theora"',
@@ -158,9 +241,62 @@
     })();
 
 
+    // TICKER
+
+    var Ticker = (function () {
+
+        var tasks = {};
+        var afId;
+        var tickerCount = 0;
+
+        function registerTask(id, task) {
+            // console.log('Ticker :: register task with id:', id);
+            tasks[id] = task;
+            startTicker();
+        }
+
+        function removeTask(id) {
+            // console.log('Ticker :: remove task with id:', id);
+            delete tasks[id];
+            if (Helpers.getObjectSize(tasks) === 0) {
+                stopTicker();
+            }
+        }
+
+        function tick() {
+            // console.log('Ticker :: tick ');
+            for (var task in tasks) {
+                tasks[task](++tickerCount);
+            }
+            afId = requestAnimationFrame(tick);
+        }
+
+        function startTicker() {
+            // console.log('Ticker :: starting ticker');
+            afId = requestAnimationFrame(tick);
+        }
+
+        function stopTicker() {
+            // console.log('Ticker :: stopping ticker');
+            // setTimeout is needed because ´tick()´ would overpass ´cancelAnimationFrame()´ => endless loop
+            setTimeout(function () {
+                window.cancelAnimationFrame(afId);
+            }, 1);
+        }
+
+        return {
+            registerTask: registerTask,
+            removeTask: removeTask
+        }
+
+    })();
+
+
     // HELPERS
 
     var Helpers = (function () {
+
+        var uniqueId = ~~(Math.random() * 1000);
 
         function domReady(fn) {
             if (document.readyState === 'complete' && window.spConfig) {
@@ -186,14 +322,21 @@
         }
 
         function doesObjectExist(obj) {
-            var i = 0;
-            if (!obj) return false;
+            return (!obj) ? -1 : getObjectSize(obj);
+        }
+
+        function getObjectSize(obj) {
+            var size = 0;
             for (var key in obj) {
                 if (obj.hasOwnProperty(key)) {
-                    i++;
+                    size++;
                 }
             }
-            return !!i;
+            return size;
+        }
+
+        function getUniqueId() {
+            return ++uniqueId + '';
         }
 
         function dispatchCustomEvent(eventName, data) {
@@ -212,6 +355,8 @@
             domReady: domReady,
             debounce: debounce,
             doesObjectExist: doesObjectExist,
+            getObjectSize: getObjectSize,
+            getUniqueId: getUniqueId,
             dispatchCustomEvent: dispatchCustomEvent,
             getAspectRatioAsPercent: getAspectRatioAsPercent
         }
@@ -219,7 +364,9 @@
     })();
 
     Helpers.domReady(function () {
-        SlidePlayerInstance.getInstance();
+        PostersInstance.getInstance().posterReady(function () {
+            SlidePlayerInstance.getInstance();
+        });
     });
 
 })(window, document);
